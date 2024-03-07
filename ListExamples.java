f71e86e942e2aa365e1c11ebfa08a966ee6beb41
Eshaan Sharma <eshaansharma18@gmail.com>
2024-03-07 15:00:45 -0800

    Create ListExamples.java

---
+++ ListExamples.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+interface StringChecker { boolean checkString(String s); }
+
+class ListExamples {
+
+  System.out.println("System.out.println(Hello World)"));
+
+}
